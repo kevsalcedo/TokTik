75569672d12bafcdf0d200fa41b339e0395f62c2
kevsalcedo <kevsalcedom@gmail.com>
2024-03-30 01:05:44 -0500

    Edit README

--- README.md
+++ README.md
@@ -4,6 +4,15 @@
 
 Tiktok-like application that plays looping videos. It has likes, views, music status and video name design. 
 
+## Note!
+
+The videos are not included in the repository, because they are very heavy and GitHub does not allow it.
+
+You can download 8 videos from here:  [Pexels Free Videos](https://www.pexels.com/search/videos/vertical/) 
+
+Rename those videos like this, since that is what is in our data source.
+
+
 ## Examples
 
 ![Example of the app.](https://imgur.com/a/QPSJlDA)
--- lib/presentation/provider/discover_provider.dart
+++ lib/presentation/provider/discover_provider.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:toktik/domain/entities/video_post.dart';
-import 'package:toktik/infrastructure/models/local_video_model.dart';
-import 'package:toktik/shared/data/local_video_post.dart';
+import 'package:toktik/domain/repositories/video_posts_repository.dart';
+//import 'package:toktik/infrastructure/models/local_video_model.dart';
+//import 'package:toktik/shared/data/local_video_post.dart';
 
 class DiscoverProvider extends ChangeNotifier {
-  // TODO: Repository, Datasource
+
+  final VideoPostRepository videosRepositoy;
 
   bool initialLoading = true;
 
   List<VideoPost> videos = [];
 
+  DiscoverProvider({required this.videosRepositoy});
+
   Future<void> loadNextPage() async {
     await Future.delayed(const Duration(seconds: 2));
 
-    final List<VideoPost> newVideos = videoPosts
+/*  final List<VideoPost> newVideos = videoPosts
         .map((video) => LocalVideoModel.fromJsonMap(video).toVideoPostEntity())
-        .toList();
+        .toList(); */
+
+    final newVideos = await videosRepositoy.getTrendingVideosByPage(1);
 
     videos.addAll(newVideos);
     initialLoading = false;
